45f8bbc4632f10dcd68c059538c4924f8d186df9
liu jiangang <1455629212@qq.com>
2016-05-24 23:23:38 +0800

    access token

---
+++ urls.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+
+class ApiUrl(object):
+	token = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"
+	pass
--- wechat.py
+++ wechat.py
@@ -2,15 +2,19 @@
 import hashlib
 from .conf import WechatConf, default_conf
 from .msg import default_reply
+from .menu import default_menu
 from .utils import xml_to_dict
+from .urls import ApiUrl
 
 
 class Wechat(object):
 
-    def __init__(self, conf=default_conf, reply_app=default_reply):
+    def __init__(self, conf=default_conf, reply_app=default_reply, \
+        menu=default_menu):
         assert isinstance(conf, WechatConf), "conf object must be a WechatConf instance!"
         self.conf = conf
         self.reply_app = reply_app
+        self._menu = menu
 
     def reply(self, method, data):
         if method.lower() == "get":
@@ -24,6 +28,22 @@ class Wechat(object):
             response = self.reply_app.response((params.get("MsgType"), params.get("Event", "")), params)
             return response
 
-    def menu(self, method, *args, **kwargs):
+    def create_menu(self):
+        self.default_menu.create(self.access_token)
         pass
+
+    def get_access_token(self):
+        access_token, expires_in = self.init_access_token()
+        pass
+
+    def init_access_token(self):
+        url = ApiUrl.token % (self.conf.appid, self.conf.appsecret)
+        resp = requests.get(url)
+        result = json.loads(resp.text)
+        if result.get("access_token"):
+            return result.get("access_token"), result.get("expires_in")
+        raise ValueError(str(result))
+
+    access_token = property(get_access_token)
+    del get_access_token
 
