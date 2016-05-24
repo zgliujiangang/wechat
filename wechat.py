ce51e0115aaf380fcdefcf470cf24fe18c3bc15b
liu jiangang <1455629212@qq.com>
2016-05-24 23:38:54 +0800

    access token 获取

--- wechat.py
+++ wechat.py
@@ -10,11 +10,40 @@ from .urls import ApiUrl
 class Wechat(object):
 
     def __init__(self, conf=default_conf, reply_app=default_reply, \
-        menu=default_menu):
+        menu=default_menu, debug=False):
         assert isinstance(conf, WechatConf), "conf object must be a WechatConf instance!"
         self.conf = conf
         self.reply_app = reply_app
         self._menu = menu
+
+    def get_access_token(self):
+        access_token = self.get_cache_access_token()
+        if not access_token:
+            access_token = self.init_access_token()
+        return access_token
+
+    def init_access_token(self):
+        url = ApiUrl.token % (self.conf.appid, self.conf.appsecret)
+        resp = requests.get(url)
+        result = json.loads(resp.text)
+        access_token = result.get("access_token")
+        expires_in = result.get("expires_in")
+        if access_token and expires_in:
+            self.cache_access_token(access_token, expires_in)
+            return access_token
+        raise ValueError(str(result))
+
+    def cache_access_token(self, access_token, expires_in):
+        # 缓存access_token 需要自定义
+        pass
+
+    def get_cache_access_token(self):
+        # 获取缓存的access_token 需要自定义
+        access_token = "1111111111"
+        return access_token
+
+    access_token = property(get_access_token)
+    del get_access_token
 
     def reply(self, method, data):
         if method.lower() == "get":
@@ -29,21 +58,6 @@ class Wechat(object):
             return response
 
     def create_menu(self):
-        self.default_menu.create(self.access_token)
-        pass
-
-    def get_access_token(self):
-        access_token, expires_in = self.init_access_token()
-        pass
-
-    def init_access_token(self):
-        url = ApiUrl.token % (self.conf.appid, self.conf.appsecret)
-        resp = requests.get(url)
-        result = json.loads(resp.text)
-        if result.get("access_token"):
-            return result.get("access_token"), result.get("expires_in")
-        raise ValueError(str(result))
-
-    access_token = property(get_access_token)
-    del get_access_token
+        result = self._menu.create(self.access_token)
+        return result
 
