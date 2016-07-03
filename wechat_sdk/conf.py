1cc1c29df4cac8fa9e6b7474d4bd3dd9b959ee29
liu jiangang <1455629212@qq.com>
2016-07-03 14:12:53 +0800

    添加微信支付回调处理类

--- wechat_sdk/conf.py
+++ wechat_sdk/conf.py
@@ -3,13 +3,14 @@
 
 class WechatConf(object):
 
-    def __init__(self, appid=None, appsecret=None, token=None, aeskey=None):
+    def __init__(self, appid=None, appsecret=None, token=None, aeskey=None, paysignkey=None):
     	assert appid is not None, "appid is none!"
     	assert appsecret is not None, "appsecret is none!"
         self.appid = appid
         self.appsecret = appsecret
         self.token = token
         self.aeskey = aeskey
+        self.paysignkey = paysignkey
 
 
 default_conf = test_conf = WechatConf(appid='wechatsdk', appsecret='wechatsdk', token="wechatsdk")
