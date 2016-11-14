c5254394e3cde981b2871a76359e98c1550a5273
liu jiangang <1455629212@qq.com>
2016-11-15 00:39:19 +0800

    'indent'

--- examples/menu_test.py
+++ examples/menu_test.py
@@ -8,21 +8,21 @@ viewButton, SubButtonGroup, MainButtonGroup, Manager
 
 
 def test():
-	menu_manager = Manager(wc)
-	click1 = clickButton(name="点击1", key="1111")
-	click2 = clickButton(name="点击2", key='2222')
-	click3 = clickButton(name="点击3", key="333")
-	view1 = viewButton(name='链接1', url='http://www.hrjia.com')
-	view2 = viewButton(name='链接2', url='http://www.baidu.com')
+    menu_manager = Manager(wc)
+    click1 = clickButton(name="点击1", key="1111")
+    click2 = clickButton(name="点击2", key='2222')
+    click3 = clickButton(name="点击3", key="333")
+    view1 = viewButton(name='链接1', url='http://www.hrjia.com')
+    view2 = viewButton(name='链接2', url='http://www.baidu.com')
 
-	group1 = SubButtonGroup('菜单1', click1, view1)
-	group2 = SubButtonGroup('菜单2', view2, click2)
+    group1 = SubButtonGroup('菜单1', click1, view1)
+    group2 = SubButtonGroup('菜单2', view2, click2)
 
-	test_menu = MainButtonGroup(group1, click3, group2)
+    test_menu = MainButtonGroup(group1, click3, group2)
 
-	# 传入一个MainButtonGroup的实例是允许的，否则的话自己构造菜单传入
-	# print menu_manager.create(test_menu)
-	print menu_manager.get()
+    # 传入一个MainButtonGroup的实例是允许的，否则的话自己构造菜单传入
+    # print menu_manager.create(test_menu)
+    print menu_manager.get()
 
 if __name__ == '__main__':
-	test()
+    test()
